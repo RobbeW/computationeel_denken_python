9eba827ccaef0d87fd3f96ff294c97b08496f740
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-06-16 14:18:56 +0200

    Add files via upload

---
+++ Hoofdstuk 1/Eenvoudige bewerkingen met twee variabelen/Oefening 1/solution/oplossing.py
@@ -0,0 +1,14 @@
+# Invoer:
+toets_1 = float(input('toets_1 = '))
+toets_2 = float(input('toets_2 = '))
+toets_3 = float(input('toets_3 = '))
+
+# Verwerking:
+totaalscore = toets_1 + toets_2 + toets_3
+gemiddelde = totaalscore / 3
+percentage = gemiddelde * 5 
+
+# Uitvoer:
+print(int(totaalscore))
+print(gemiddelde)
+print(percentage)
