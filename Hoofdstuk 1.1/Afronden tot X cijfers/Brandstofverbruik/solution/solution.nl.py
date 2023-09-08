a48c40f6d3ed7cd23f44f9fc69183c781e5cb20a
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-09-08 07:24:46 +0200

    Update solution.nl.py
    Verwijderen format-notatie en UTF markers

--- Hoofdstuk 1.1/Afronden tot X cijfers/Brandstofverbruik/solution/solution.nl.py
+++ Hoofdstuk 1.1/Afronden tot X cijfers/Brandstofverbruik/solution/solution.nl.py
@@ -1,8 +1,5 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 afstand = float( input( 'Geef een afstand (in km) in: ' ) )
 
 verbruik = round( afstand * 4.5 / 100, 2 )
 
-print('{} liter'.format( verbruik ) )
+print(verbruik, 'liter')
