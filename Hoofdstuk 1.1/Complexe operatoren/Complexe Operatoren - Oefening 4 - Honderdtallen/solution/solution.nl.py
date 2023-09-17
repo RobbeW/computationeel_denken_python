0b48d603a79d431c0e9446c00f36810fef757545
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-09-17 23:19:01 +0200

    Solution fix

--- Hoofdstuk 1.1/Complexe operatoren/Complexe Operatoren - Oefening 4 - Honderdtallen/solution/solution.nl.py
+++ Hoofdstuk 1.1/Complexe operatoren/Complexe Operatoren - Oefening 4 - Honderdtallen/solution/solution.nl.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
+# Vraag een getal aan de gebruiker
 getal = int( input( 'Geef een getal in: ' ) )
 
+# Bereken de honderdtallen
 honderdtallen = ( getal // 100 ) % 10
 
-print('\n{}'.format( honderdtallen ) )
+# Druk deze op het scherm af
+print( honderdtallen )
