2cec4e014940cebdffd16afe4217ca686f28ad7d
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-10-08 16:34:10 +0200

    Final fix

--- Hoofdstuk 2.1/2-Werken met IF en ELSE/Werken met IF en ELSE - Oefening 1 - Gratis Verzending Deel 2/solution/solution.nl.py
+++ Hoofdstuk 2.1/2-Werken met IF en ELSE/Werken met IF en ELSE - Oefening 1 - Gratis Verzending Deel 2/solution/solution.nl.py
@@ -1,10 +1,9 @@
 # Vraag de gebruiker naar het aankoopbedrag
 aankoopbedrag = float(input("Voer het aankoopbedrag in: "))
-print()
 
 # Bepaal of de gebruiker in aanmerking komt voor gratis verzending
 if aankoopbedrag >= 20:
-    print("Je komt in aanmerking voor gratis verzending. Het totaalbedrag is", aankoopbedrag," euro.")
+    print("Je komt in aanmerking voor gratis verzending. Het totaalbedrag is", aankoopbedrag,"euro.")
 else:
     totaalbedrag = aankoopbedrag + 4
     print("Je komt niet in aanmerking voor gratis verzending. Het totaalbedrag inclusief verzending is", totaalbedrag, "euro.")
