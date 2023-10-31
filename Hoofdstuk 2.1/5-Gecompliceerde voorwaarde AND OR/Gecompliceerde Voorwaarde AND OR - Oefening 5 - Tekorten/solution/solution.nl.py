b19de33b4c40af4668f6fd6611b6337fd2ea6c73
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-10-31 12:56:42 +0100

    Fixing tekorten to yaml

---
+++ Hoofdstuk 2.1/5-Gecompliceerde voorwaarde AND OR/Gecompliceerde Voorwaarde AND OR - Oefening 5 - Tekorten/solution/solution.nl.py
@@ -0,0 +1,12 @@
+# Vraag de gebruiker om de scores voor de twee toetsen
+score1 = int( input("Voer de score van de eerste test in: " ) )
+score2 = int( input("Voer de score van de tweede test in: " ) )
+
+# Beoordeel de scores
+print()
+if score1 >= 50 and score2 >= 50:
+    print("Beoordeling:", "oké")
+elif score1 < 50 and score2 < 40:
+    print("Beoordeling:", "niet oké")
+else:
+    print("Beoordeling:", "één tekort")
