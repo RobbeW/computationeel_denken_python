6e9f5a512242698d640c51caaea963d6277f4344
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-10-31 10:53:54 +0100

    Solution fix

--- Hoofdstuk 2.1/4-Gecompliceerde voorwaarde gelijk aan niet gelijk aan/Oefening 3 - Stappenteller/solution/solution.nl.py
+++ Hoofdstuk 2.1/4-Gecompliceerde voorwaarde gelijk aan niet gelijk aan/Oefening 3 - Stappenteller/solution/solution.nl.py
@@ -2,9 +2,9 @@ aantalStappen = int( input( 'Hoeveel stappen moet je nog zetten?' ) )
 
 teDoen = 10000 - aantalStappen
 
-if( aantalStappen < 9999 ):
+if aantalStappen < 9999 :
     print('Je dient nog', teDoen, 'stappen te zetten.')
-elif (aantalStappen == 9999):
+elif aantalStappen == 9999 :
     print('Je moet maar één stap meer zetten.')
 else: 
     print('Je hebt je stappendoel van vandaag reeds bereikt.')
