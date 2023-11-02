7c48c848b70fe441fba77fb16d34a83aa2a79412
Dieter Vanderfaeillie <dieter.vanderfaeillie@sintlievenscollege.be>
2023-11-02 21:18:09 +0100

    Fix concat + remove hack

--- Hoofdstuk 3.1/1 - Opstellen Herhaling Optellen/Oefening 1 - Som van Tien Getallen/solution/solution.nl.py
+++ Hoofdstuk 3.1/1 - Opstellen Herhaling Optellen/Oefening 1 - Som van Tien Getallen/solution/solution.nl.py
@@ -7,5 +7,4 @@ for i in range(10):
     som += getal  # voeg het ingevoerde getal toe aan de som
 
 # print de som van de getallen
-print()
-print("De som van de getallen is", som)
+print("De som van de getallen is", str(som)+".")
