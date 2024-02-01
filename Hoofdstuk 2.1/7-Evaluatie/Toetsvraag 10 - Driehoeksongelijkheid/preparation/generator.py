3d790cb77be79fea11b6017538789e5e4bac735c
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-02-01 23:08:43 +0100

    Fixing tests

--- Hoofdstuk 2.1/7-Evaluatie/Toetsvraag 10 - Driehoeksongelijkheid/preparation/generator.py
+++ Hoofdstuk 2.1/7-Evaluatie/Toetsvraag 10 - Driehoeksongelijkheid/preparation/generator.py
@@ -29,7 +29,7 @@ comparison: exact match
 
 # generate test data
 ntests= 20
-cases = [(4,7,2), (4,5,2)]
+cases = [(4,7,2), (4,5,2), (5,2,3), (2,5,3), (2,3,5)]
 while len(cases) < ntests:
     cases.append( tuple(random.randint(2,12) for _ in range(3)) ) 
 
