22861fb3b938bc0c406a0de8ff02d2dd1ca72c82
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-02-15 01:53:03 +0100

    Fixing list exercise

--- Hoofdstuk 3.1/3 - Intro Lijsten en Append/Oefening 1 - Verkoopsaantallen/solution/solution.nl.py
+++ Hoofdstuk 3.1/3 - Intro Lijsten en Append/Oefening 1 - Verkoopsaantallen/solution/solution.nl.py
@@ -8,9 +8,11 @@ som = 0
 for vestiging in vestigingen:
     # Vraag de gebruiker om het verkoopbedrag voor deze vestiging
     bedrag = float(input("Voer het totaal in voor de winkel in " + vestiging + ": "))
+    print()
     
     # Voeg het bedrag toe aan de som
     som += bedrag
 
 # Print het totale bedrag naar het scherm
-print("Het maandtotaal bedraagt", som, "euro.")
+
+print("Het maandtotaal bedraagt", round(som, 2), "euro.")
