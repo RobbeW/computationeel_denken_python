8ec6ba9b727536fb80ff38c0660bb43d0d94ed43
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-04-25 11:32:15 +0200

    Fixing solution

--- Hoofdstuk 3.1/6 - Evaluatie/Rechthoekig getal/solution/solution.nl.py
+++ Hoofdstuk 3.1/6 - Evaluatie/Rechthoekig getal/solution/solution.nl.py
@@ -2,10 +2,9 @@ getal = int( input( "Geef een getal in: " ) )
 
 rechthoekig = False
 for i in range(1,getal):
-    if getal % i == 0 and getal % (i+1) == 0 and i*(i+1) == getal:
+    if i*(i+1) == getal:
         rechthoekig = True
 
-print()
 if rechthoekig:
     print(getal, "is een rechthoekig getal.")
 else:
