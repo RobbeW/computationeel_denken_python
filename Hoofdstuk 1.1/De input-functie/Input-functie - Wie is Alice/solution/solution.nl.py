74752916e1238cf3e75651f0111f4d2038e46cda
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-08-03 11:44:30 +0200

    fix solution

--- Hoofdstuk 1.1/De input-functie/Input-functie - Wie is Alice/solution/solution.nl.py
+++ Hoofdstuk 1.1/De input-functie/Input-functie - Wie is Alice/solution/solution.nl.py
@@ -1,11 +1,5 @@
-# Maak een variabele met de naam bericht en wijs er de string "Hallo, mijn naam is " aan toe
-bericht = "Hallo, mijn naam is "
-
 # Vraag de gebruiker om zijn/haar/hun naam in te voeren
 naam = input("Voer je naam in: ")
 
-# Voeg de ingevoerde naam toe aan de variabele bericht
-bericht = bericht + naam
-
 # Print de inhoud van de variabele bericht naar het scherm
-print(bericht)
+print("Hallo, mijn naam is", naam)
