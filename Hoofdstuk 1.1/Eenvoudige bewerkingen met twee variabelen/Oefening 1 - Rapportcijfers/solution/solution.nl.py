14870a4ae20d3d51d0ad8349b10bc1c214065a59
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-08-05 02:22:15 +0200

    Fixing solution

--- Hoofdstuk 1.1/Eenvoudige bewerkingen met twee variabelen/Oefening 1 - Rapportcijfers/solution/solution.nl.py
+++ Hoofdstuk 1.1/Eenvoudige bewerkingen met twee variabelen/Oefening 1 - Rapportcijfers/solution/solution.nl.py
@@ -1,7 +1,7 @@
 # Vraag de gebruiker om de score van drie toetsen in te voeren
-toets_1 = float(input("toets_1 = "))
-toets_2 = float(input("toets_2 = "))
-toets_3 = float(input("toets_3 = "))
+toets_1 = float(input("Geef je resultaat voor toets 1: "))
+toets_2 = float(input("Geef je resultaat voor toets 2: "))
+toets_3 = float(input("Geef je resultaat voor toets 3: "))
 
 # Bereken de totaalscore
 totaalscore = toets_1 + toets_2 + toets_3
@@ -13,6 +13,6 @@ gemiddelde = totaalscore / 3
 percentage = gemiddelde * 5
 
 # Print de totaalscore, het gemiddelde en het percentage naar het scherm
-print(totaalscore)
-print(gemiddelde)
-print(percentage)
+print("Totaal:", totaalscore)
+print("Gemiddelde:", gemiddelde)
+print("Percentage:", percentage)
