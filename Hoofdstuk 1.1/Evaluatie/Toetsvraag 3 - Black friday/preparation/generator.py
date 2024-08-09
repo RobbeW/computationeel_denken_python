1ab90ef9a8bfa4a0246fb78696bb262db5cc8e6c
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-08-09 19:14:16 +0200

    TESTed transform

--- Hoofdstuk 1.1/Evaluatie/Toetsvraag 3 - Black friday/preparation/generator.py
+++ Hoofdstuk 1.1/Evaluatie/Toetsvraag 3 - Black friday/preparation/generator.py
@@ -1,10 +1,15 @@
 import os
+import sys
+import importlib.util
 import random
+import ruamel.yaml
 import subprocess
-from typing import Text
+from math import ceil, floor
+
+yaml = ruamel.yaml.YAML()
 
 # set fixed seed for generating test cases
-random.seed(123456789)
+random.seed(12345678)
 
 # locate evaldir
 evaldir = os.path.join('..', 'evaluation')
@@ -16,34 +21,49 @@ solutiondir = os.path.join('..', 'solution')
 if not os.path.exists(solutiondir):
     os.makedirs(solutiondir)
 
-# configuration settings
-tab_name = 'Feedback'
-settings = f'''
-tab name: {tab_name}
-python input without prompt: true
-block count: multi
-input block size: 2
-output block size: ends with
-comparison: exact match
-'''
+def write_yaml( data:list ):
+    """ A function to write YAML file"""
+    with open(os.path.join('..', 'evaluation', 'tests.yaml'), 'w', encoding='utf-8') as f:
+        yaml.dump(data, f)
+
+
+module_name = 'solution'
+file_path = os.path.join(solutiondir, 'solution.nl.py')
+spec = importlib.util.spec_from_file_location(module_name, file_path)
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
+
 
 # generate test data
-ntests= 20
-kortingen = [15, 25, 30, 50, 60, 75]
-cases = [(134.4,50),]
+ntests = 20
+kortingen = [15, 25, 30, 30, 30,  50, 60, 75] # Extra's 30 to force to pick 30% below, a bit hacky
+cases = [(134.4, 50),]
 while len(cases) < ntests:
-    cases.append( (round( random.uniform(50, 150), 0), random.choice(kortingen)) )
+    korting = random.choice(kortingen)
+    bedrag = round(random.uniform(50, 150), 1)
+    calc1 = bedrag * (100 - korting) / 100
+    calc2 = bedrag * ( 1 - korting / 100 )
+    calc3 = bedrag - bedrag * korting / 100
+    if floor(calc1 * 10) == ceil(calc1 * 10)  and floor(calc2) == ceil(calc2) and floor(calc3) == ceil(calc3) and (bedrag, korting) not in cases:
+        cases.append( (bedrag, korting) )
 
-# configure test files
-infile = open(os.path.join(evaldir, '0.in'), 'w')
-outfile = open(os.path.join(evaldir, '0.out'), 'w')
+# generate unit tests for functions
+yamldata = []
 
-# generate unit tests
-for stdin in cases:
+# input, expression, statement or stdin?
+input = 'stdin'
+# output, stdout or return?
+output = 'stdout'
+tabtitle = "Feedback"
 
+yamldata.append( {'tab': tabtitle, 'contexts': []})
+
+for i in range(len(cases)):
+    test = cases[i]
+    yamldata[0]['contexts'].append( {'testcases' : []})
+    # generate test expression
     # add input to input file
-    stdin = '\n'.join(f'{line}' for line in stdin)
-    print(stdin, file=infile)
+    stdin = '\n'.join(f'{line}' for line in test)
 
     # generate output to output file
     script = os.path.join(solutiondir, 'solution.nl.py')
@@ -53,15 +73,18 @@ for stdin in cases:
         encoding='utf-8',
         capture_output=True
     )
-        
+    
     result_lines = process.stdout.split("\n")
+    result_lines = [x for x in result_lines[:-1]] ## drop last element
+    
+    outputtxt = ""
     for line in result_lines:
-        if not(line.startswith( 'Geef' )):
+        if not(line.startswith( 'Geef' ) or line.startswith( 'Voer' )):
             print(line)
-            print(line, file=outfile)
+            outputtxt += line + "\n"
+            
+    testcase = { input: stdin, output: outputtxt }            
+    yamldata[0]['contexts'][i]["testcases"].append( testcase)
 
-    # add stdout to output file
-    # print(process.stdout, file=outfile, end='')
 
-# add settings to output file
-print('-' * 41 + settings, file=outfile, end='')
+write_yaml(yamldata)
