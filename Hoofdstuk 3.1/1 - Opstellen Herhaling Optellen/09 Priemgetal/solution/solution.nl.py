ce5bac3d17a97a77056100e3030c37ba31ca3d19
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-08-10 19:22:45 +0200

    Switch to TESTed

--- Hoofdstuk 3.1/1 - Opstellen Herhaling Optellen/09 Priemgetal/solution/solution.nl.py
+++ Hoofdstuk 3.1/1 - Opstellen Herhaling Optellen/09 Priemgetal/solution/solution.nl.py
@@ -1,7 +1,6 @@
 # Vraag een getal
 getal = int( input( "Geef de getal in: " ) )
 
-
 niet_priem = False # Ga ervan uit dat het getal priem is
 for d in range(2, getal):
     niet_priem = niet_priem or getal % d == 0
