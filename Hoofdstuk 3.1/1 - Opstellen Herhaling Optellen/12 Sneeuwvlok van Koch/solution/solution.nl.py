0fa8cb964d6b890613dacc0f943a5516bedebbbd
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-08-10 19:38:23 +0200

    Switch to TESTed

--- Hoofdstuk 3.1/1 - Opstellen Herhaling Optellen/12 Sneeuwvlok van Koch/solution/solution.nl.py
+++ Hoofdstuk 3.1/1 - Opstellen Herhaling Optellen/12 Sneeuwvlok van Koch/solution/solution.nl.py
@@ -3,7 +3,6 @@ z = float( input( 'Geef de zijde in (in cm): ' ) )
 n = int( input( 'Geef de iteratie in: ' ) )
 
 # Oppervlakte berekenen
-print()
 P = 3 * z
 print("De startomtrek was", round(P, 2), "cm.")
 for i in range(n):
