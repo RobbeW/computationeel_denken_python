3b6a6d669dee17aa799feaefee4faf24e45f2afa
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-08-11 12:16:21 +0200

    Major refactor

--- 01 Sequentie/Evaluatie/Toetsvraag 11 - Concorde/solution/solution.nl.py
+++
@@ -1,9 +0,0 @@
-# Invoer vragen
-aantal_min = int( input("Hoeveel minuten zal je met de Concorde vliegen? ") )
-
-# Verwerking
-aantal_uur = aantal_min / 60
-afstand = aantal_uur * 1740
-
-# Weergeven op het scherm
-print("Als je", aantal_min, "minuten vliegt, dan heb je", afstand, "km afgelegd.")
