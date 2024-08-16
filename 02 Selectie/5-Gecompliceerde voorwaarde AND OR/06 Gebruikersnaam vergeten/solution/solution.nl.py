987a188876dca37091747964ee0782e7dcb7d9b2
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2024-08-16 16:51:43 +0200

    Update solution.nl.py
    Aanpassen oplossingsleutel. Deze oefening steekt in de reeks met and / or, maar de sleutel gebruikte geneste if / elif / else statements zonder and / or.

--- 02 Selectie/5-Gecompliceerde voorwaarde AND OR/06 Gebruikersnaam vergeten/solution/solution.nl.py
+++ 02 Selectie/5-Gecompliceerde voorwaarde AND OR/06 Gebruikersnaam vergeten/solution/solution.nl.py
@@ -2,15 +2,14 @@
 correct_emailadres = "janedoe@sintlievenscollege.be"
 correcte_gebruikersnaam = "janedoe"
 
-# Vraag de gebruiker om de gebruikersnaam en wachtwoord
+# Vraag de gebruiker om de gebruikersnaam en het emailadres
 ingevoerd_emailadres = input("Voer uw emailadres in: ")
 ingevoerde_gebruikersnaam = input("Voer uw gebruikersnaam in: ")
 
 # Controleer de invoer
 if correct_emailadres != ingevoerd_emailadres:
     print("Emailadres niet gekend.")
-else:
-    if correcte_gebruikersnaam == ingevoerde_gebruikersnaam:
-        print("Resetlink verstuurd.")
-    else:
-        print("Gebruiker en email komen niet overeen.")
+elif correct_emailadres == ingevoerd_emailadres and correcte_gebruikersnaam != ingevoerde_gebruikersnaam:
+    print("Gebruiker en email komen niet overeen.")
+elif correct_emailadres == ingevoerd_emailadres and correcte_gebruikersnaam == ingevoerde_gebruikersnaam:
+    print("Resetlink verstuurd.")
