5ab0f2100f76a42013e6c066ef80a1c845cf2b3c
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2024-09-28 12:42:21 +0200

    Update solution.nl.py

--- 02 Selectie/3-Werken met IF, ELIF, ELSE/04 Risk/solution/solution.nl.py
+++ 02 Selectie/3-Werken met IF, ELIF, ELSE/04 Risk/solution/solution.nl.py
@@ -1,4 +1,4 @@
-# Invoer aanvaller
+# Invoer aanvaller opvragen en bewaren in drie variabelen: 
 A1 = int(input("Geef de eerste worp van de aanvaller in: "))
 A2 = int(input("Geef de tweede worp van de aanvaller in: "))
 A3 = int(input("Geef de derde worp van de aanvaller in: "))
@@ -10,28 +10,30 @@ A_eerste = max(A1, A2, A3)
 # Tweede hoogste
 # Bereken de som van alle worpen
 A_som = A1 + A2 + A3
-# Bereken de tweede hoogste door de hoogste en laagste worp af te trekken van de som
+# Bereken de tweede hoogste door de hoogste en laagste worp af te trekken van de som, waardoor de middelste waarde overblijft. 
 A_tweede = A_som - A_eerste - min(A1, A2, A3)
 
-# Invoer verdediger
+# Invoer verdediger opvragen en bewaren in twee variabelen:
 V1 = int(input("Geef de eerste worp van de verdediger in: "))
 V2 = int(input("Geef de tweede worp van de verdediger in: "))
 
-# Bepaal de hoogste en tweede hoogste worp van de verdediger
+# Bepaal de hoogste en tweede hoogste worp van de verdediger via max() en min(): 
 V_eerste = max(V1, V2)
 V_tweede = min(V1, V2)
 
-# Initialiseer verliezen
+# We maken twee tellers aan om de verliezen bij te houden: 
 aanvaller_verlies = 0
 verdediger_verlies = 0
 
 # Vergelijk de hoogste worpen
+# Belangrijk: enkel wanneer de aanvaller HOGER rolt, tikken de verliezen van de verdediger aan: 
 if A_eerste > V_eerste:
     verdediger_verlies += 1
 else:
     aanvaller_verlies += 1
 
 # Vergelijk de tweede hoogste worpen
+# Belangrijk: enkel wanneer de aanvaller HOGER rolt, tikken de verliezen van de verdediger aan: 
 if A_tweede > V_tweede:
     verdediger_verlies += 1
 else:
