8847f272ab34370e568b4c1cb89928ee2be7fc20
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2024-09-28 12:13:16 +0200

    Update solution.nl.py
    Herwerken solution, logica werd omgekeerd uitgevoerd.

--- 02 Selectie/3-Werken met IF, ELIF, ELSE/04 Risk/solution/solution.nl.py
+++ 02 Selectie/3-Werken met IF, ELIF, ELSE/04 Risk/solution/solution.nl.py
@@ -1,26 +1,42 @@
-# Invoer speler 1
-P1_1 = int(input("Geef de eerste worp van speler 1 in: "))
-P1_2 = int(input("Geef de tweede worp van speler 1 in: "))
-P1_3 = int(input("Geef de derde worp van speler 1 in: "))
+# Invoer aanvaller
+A1 = int(input("Geef de eerste worp van de aanvaller in: "))
+A2 = int(input("Geef de tweede worp van de aanvaller in: "))
+A3 = int(input("Geef de derde worp van de aanvaller in: "))
 
-# Bepaalde hoogste en tweede hoogste worp
-P1_eerste = max(P1_1, P1_2, P1_3)
-P1_tweede = P1_1 + P1_2 + P1_3 - max(P1_1, P1_2, P1_3) - min(P1_1, P1_2, P1_3)
+# Bepaal de hoogste en tweede hoogste worp van de aanvaller
+# Eerste hoogste
+A_eerste = max(A1, A2, A3)
 
-# Invoer speler 2
-P2_1 = int(input("Geef de eerste worp van speler 2 in: "))
-P2_2 = int(input("Geef de tweede worp van speler 2 in: "))
+# Tweede hoogste
+# Bereken de som van alle worpen
+A_som = A1 + A2 + A3
+# Bereken de tweede hoogste door de hoogste en laagste worp af te trekken van de som
+A_tweede = A_som - A_eerste - min(A1, A2, A3)
 
-# Stel het aantal verloren eenheden in op 0
-P1_verloren = 0
+# Invoer verdediger
+V1 = int(input("Geef de eerste worp van de verdediger in: "))
+V2 = int(input("Geef de tweede worp van de verdediger in: "))
 
-# Wijzig dit aantal, naargelang de worpen
-if P1_eerste >= max(P2_1, P2_2):
-    P1_verloren += 1
+# Bepaal de hoogste en tweede hoogste worp van de verdediger
+V_eerste = max(V1, V2)
+V_tweede = min(V1, V2)
 
-if P1_tweede >= min(P2_1, P2_2):
-    P1_verloren += 1
+# Initialiseer verliezen
+aanvaller_verlies = 0
+verdediger_verlies = 0
+
+# Vergelijk de hoogste worpen
+if A_eerste > V_eerste:
+    verdediger_verlies += 1
+else:
+    aanvaller_verlies += 1
+
+# Vergelijk de tweede hoogste worpen
+if A_tweede > V_tweede:
+    verdediger_verlies += 1
+else:
+    aanvaller_verlies += 1
 
 # Uitvoer
-print("Verloren eenheden aanvaller:", P1_verloren)
-print("Verloren eenheden verdediging:", 2 - P1_verloren)
+print("Verloren eenheden aanvaller:", aanvaller_verlies)
+print("Verloren eenheden verdediging:", verdediger_verlies)
