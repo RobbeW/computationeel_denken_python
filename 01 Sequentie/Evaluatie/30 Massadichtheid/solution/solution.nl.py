a57735886edaac0769b2d4ea5ba00c1dcaa5f3ec
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2024-10-09 23:29:49 +0200

    typo fix

--- 01 Sequentie/Evaluatie/30 Massadichtheid/solution/solution.nl.py
+++ 01 Sequentie/Evaluatie/30 Massadichtheid/solution/solution.nl.py
@@ -1,6 +1,6 @@
 # Invoer vragen
 massa = float( input("Geef de massa in gram in? ") )
-volume = float( input("Geef de het volume in m³ in? ") )
+volume = float( input("Geef de het volume in dm³ in? ") )
 
 # Verwerking
 dichtheid = massa / (volume / 1000)
