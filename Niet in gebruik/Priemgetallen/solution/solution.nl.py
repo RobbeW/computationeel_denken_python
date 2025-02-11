7c3ecff2093536af0487f7b8cf18f21c49aaee8b
Exy Sabbe <exy.sabbe@sintlievenscollege.be>
2025-02-11 15:05:40 +0100

    Fix nieuwe oef

--- Niet in gebruik/Priemgetallen/solution/solution.nl.py
+++ Niet in gebruik/Priemgetallen/solution/solution.nl.py
@@ -1,1 +1,12 @@
-print("Probeer deze code zelf uit in de Sandbox!")
+def is_priem(getal):
+    i = 2
+    while i < getal and getal % i != 0:
+        i = i + 1
+    # Indien geen delers werden gevonden is i == getal
+    return i == getal
+
+get = int(input())
+if is_priem(get):
+    print('Y')
+else:
+    print('N')
