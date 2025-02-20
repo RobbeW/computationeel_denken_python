5fd31d357533dd018d9482b14795deaa784c320d
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2025-02-20 23:02:17 +0100

    Fixing tests

--- 03 Begrensde herhaling/1 - Opstellen herhaling/15 Coordinaten vierkant weergeven/solution/solution.nl.py
+++ 03 Begrensde herhaling/1 - Opstellen herhaling/15 Coordinaten vierkant weergeven/solution/solution.nl.py
@@ -1,6 +1,5 @@
 zijde = int(input("Geef de zijde van het vierkant in: "))
 
-print()
 print("Een vierkant met een zijde van", zijde, "bevat deze punten:")
 for i in range(zijde):
     for j in range(zijde):
