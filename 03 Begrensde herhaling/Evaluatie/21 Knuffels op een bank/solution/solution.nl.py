4f81662215218e139582dd78532a645d355e49c0
DieterPi <dieter.vanderfaeillie@sintlievenscollege.be>
2025-03-04 14:08:20 +0100

    Initial rhyme exercise

---
+++ 03 Begrensde herhaling/Evaluatie/21 Knuffels op een bank/solution/solution.nl.py
@@ -0,0 +1,5 @@
+aantal = int( input( "Geef het aantal strofes in: " ) )
+print()
+for i in range(aantal): 
+    print(i + 2, "knuffels zitten netjes op een rij")
+    print("Eentje fluistert zachtjes: Mag ik er ook nog bij?")
